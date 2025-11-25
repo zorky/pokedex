61c678c929f95fdbc1f2a1ac1e8c2d4d098e7231
Olivier DUVAL <zorky00@gmail.com>
2025-11-25 13:23:29 +0100

    modification port

--- app.py
+++ app.py
@@ -480,5 +480,5 @@ async def shutdown():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8377, reload=True)
     
